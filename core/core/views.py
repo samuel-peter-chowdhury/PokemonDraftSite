491cf4249b6af284376b1ddeccc51e4ab273c626
samuel-peter-chowdhury <samuel.peter.chowdhury@gmail.com>
2024-11-26 14:59:22 -0500

    Add user settings page

--- core/core/views.py
+++ core/core/views.py
@@ -1,13 +1,6 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="/users/login/")
 def homepage(request):
-    # return HttpResponse("Hello World! I'm Home.")
     return render(request, 'home.html')
-
-@login_required(login_url="/users/login/")
-def about(request):
-    # return HttpResponse("My About page.")
-    return render(request, 'about.html')
--- core/users/views.py
+++ core/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect 
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
+from django.contrib.auth.forms import AuthenticationForm 
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Create your views here.
 def register_view(request):
@@ -10,7 +12,10 @@ def register_view(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             login(request, form.save())
-            return redirect("/")
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("/")
     else:
         form = CustomUserCreationForm()
     return render(request, "users/register.html", { "form": form })
@@ -28,7 +33,20 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, "users/login.html", { "form": form })
 
+@login_required(login_url="/users/login/")
 def logout_view(request):
     if request.method == "POST": 
         logout(request) 
         return redirect("users:login")
+
+@login_required(login_url="/users/login/")
+def settings_view(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successful Update')
+            return redirect("users:settings")
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    return render(request, "users/settings.html", { "form": form })
